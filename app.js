415eb609b5a8396274dc321c6c17aca599c5b19c
tayyabhussain2910 <tayyabfaisal106@gmail.com>
2021-04-10 21:50:12 +0500

    chapter5 task claculation section complete

---
+++ app.js
@@ -0,0 +1,22 @@
+// **********CHAPTER 4-8 TASK**********
+// MATH EXPRESSIONS
+
+// *****CALCULATION SECTION*****
+
+// var a = 5
+// var b = 25
+// // var addition = a + b
+// // document.write(addition)
+
+// // var subtraction = b - a
+// // document.write(subtraction)
+
+// // var multiplication = a * b
+// // document.write(multiplication)
+
+// // var division = b / a
+// // document.write(division)
+
+// var modulus = a % b
+// document.write(modulus)
+
